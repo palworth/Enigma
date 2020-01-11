1e9505f8b8e8895af0d7cc59d14c0808e9a74d03
Pierce Alworth <pbalworth@gmail.com>
2020-01-11 10:22:19 -0700

    Adding partial functionality of offset test

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -4,10 +4,17 @@ require 'date'
 
 class ShiftTest < Minitest::Test
   def setup
-    @enigma = Enigma.new
+    # @enigma = Enigma.new
     @shift = Shift.new
   end
+
   def test_it_exists
-    assert_instance_of Shift, @shift 
+    assert_instance_of Shift, @shift
   end
+
+  def test_it_returns_final_offset_split
+    require "pry"; binding.pry
+    assert_equal 1, @shift.offset
+  end
+
 end
