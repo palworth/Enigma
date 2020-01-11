47839a1482df3c3a7e6f9132b0f70f1255342833
Pierce Alworth <pbalworth@gmail.com>
2020-01-11 10:43:19 -0700

    Adding functional offset with one bug. Adding lib

--- lib/shift.rb
+++ lib/shift.rb
@@ -12,31 +12,24 @@ class Shift
  # end
 
   def offset(date = nil)
-    date_changed =
-    if date == nil
-       @date_today**2
-    else
-      date.to_i**2
-    end
-     date_changed.to_s.split(//).map{|number| number.to_i}
-
-    # {
-    #   a: final_offset_split[-4],
-    #   b: final_offset_split[-3],
-    #   c: final_offset_split[-2],
-    #   d: final_offset_split[-1]
-    # }
+    {
+      a: final_offset_split(date)[-4],
+      b: final_offset_split(date)[-3],
+      c: final_offset_split(date)[-2],
+      d: final_offset_split(date)[-1]
+    }
 
   end
 
   def final_offset_split(date = nil)
-    date_changed =
+    # require "pry"; binding.pry
+
     if date == nil
        @date_today**2
     else
+      # require "pry"; binding.pry
       date.to_i**2
-    end
-     date_changed.to_s.split(//).map{|number| number.to_i}
+    end.to_s.split(//).map{|number| number.to_i}
   end
 
 
