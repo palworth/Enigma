fae9c35a73e358a1bd822c8572f448fc9aee6633
Pierce Alworth <pbalworth@gmail.com>
2020-01-11 09:09:20 -0700

    Adding date functionality, finds current date.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,13 @@
 class Enigma
+  attr_reader :date, :date_today
+
+  def initialize
+    @date = date
+    @date_today = Time.now.strftime("%m/%d/%y").gsub("/", "").to_i
+  end
+
+  def encrypt(message, key, date)
+    require "pry"; binding.pry
+  end
 
 end
