ee077970dafff18d18e0c99822140cf0c95d4697
Pierce Alworth <pbalworth@gmail.com>
2020-01-11 10:42:49 -0700

    Offset function with one bug, adding test

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -12,9 +12,12 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, @shift
   end
 
-  def test_it_returns_final_offset_split
-    require "pry"; binding.pry
-    assert_equal 1, @shift.offset
+  def test_it_returns_final_offset_split_array
+    assert_equal [1, 2, 3, 6, 5, 4, 4, 0, 0], @shift.final_offset_split
+  end
+
+  def test_final_offset_split_array_changes_with_argument
+    assert_equal [1, 6, 9, 5, 7, 2, 4, 8, 4, 0, 0], @shift.final_offset_split(130220)
   end
 
 end
