b7fb2e8bbf2e828d51b0d8dcf865001af48dfe7e
Pierce Alworth <pbalworth@gmail.com>
2020-01-11 09:08:40 -0700

    Adding date functionality test.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,6 @@
 require_relative '../test_helper'
 require './lib/enigma'
+require 'date'
 
 class EnigmaTest < Minitest::Test
   def setup
@@ -10,4 +11,23 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_returns_date
+    assert_equal 11120, @enigma.date_today
+  end
+
+  def test_it_encrypts_message
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
 end
+
+
+# character_set = ("a".."z").to_a << " "
+#
+# @timestamp = Time.now
+# @timestamp=2020-01-09 19:09:27
