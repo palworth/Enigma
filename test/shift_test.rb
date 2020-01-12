9addd9c3b760fac94c55e679ae7638854cdeda7a
Pierce Alworth <pbalworth@gmail.com>
2020-01-12 09:01:21 -0700

    Fixing date/offset bug in shift. Commiting test

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -12,12 +12,16 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, @shift
   end
 
+  def test_it_returns_date
+    assert_equal 120120, @shift.date_today
+  end
+
   def test_it_returns_final_offset_split_array
-    assert_equal [1, 2, 3, 6, 5, 4, 4, 0, 0], @shift.final_offset_split
+    assert_equal [1, 4, 4, 2, 8, 8, 1, 4, 4, 0, 0], @shift.final_offset_split
   end
 
   def test_final_offset_split_array_changes_with_argument
-    assert_equal [1, 7, 4, 7, 6, 8, 4, 0, 0], @shift.final_offset_split("13220")
+    assert_equal [1, 6, 9, 5, 7, 2, 4, 8, 4, 0, 0], @shift.final_offset_split("130220")
   end
 
   def test_it_calculates_offset_hash_with_set_date
@@ -26,8 +30,8 @@ class ShiftTest < Minitest::Test
   end
 
   def test_it_calculates_offset_hash
-    expected = {:a=>9, :b=>9, :c=>3, :d=>6}
-    assert_equal expected, @shift.offset("01456")
+    expected = {:a=>4, :b=>4, :c=>0, :d=>0}
+    assert_equal expected, @shift.offset("010120")
   end
 
   def test_it_returns_generated_key
@@ -43,8 +47,8 @@ class ShiftTest < Minitest::Test
   end
 
   def test_it_returns_key
-    expected = {:a=>12, :b=>23, :c=>34, :d=>45}
-    assert_equal expected, @shift.key("12345")
+    expected = {:a=>02, :b=>23, :c=>34, :d=>45}
+    assert_equal expected, @shift.key("02345")
   end
 
 
