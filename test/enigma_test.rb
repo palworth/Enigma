666f0178626e2031deeb57d56a1d088db2b2390b
Pierce Alworth <pbalworth@gmail.com>
2020-01-11 17:10:31 -0700

    Adding message functionality. Test.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,10 +1,12 @@
 require_relative '../test_helper'
 require './lib/enigma'
+require './lib/shift'
 require 'date'
 
 class EnigmaTest < Minitest::Test
   def setup
     @enigma = Enigma.new
+    @shift = Shift.new
   end
 
   def test_it_exists
@@ -21,7 +23,11 @@ class EnigmaTest < Minitest::Test
       key: "02715",
       date: "040895"
     }
-    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+    assert_equal expected, @enigma.encrypt("hello world", "12715", "040895")
+  end
+
+  def test_it_can_isolate_messages
+    assert_equal [["h", "e", "l", "l"], ["o", " ", "w", "o"], ["r", "l", "d"]], @enigma.msg_isolator("hello world")
   end
 
 end
