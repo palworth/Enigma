72cf7ffd8c579ac9be6af6e5f4362a8420033168
Pierce Alworth <pbalworth@gmail.com>
2020-01-12 11:11:38 -0700

    Adding decryption method. Functioning with key and date. Commiting lib file

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -17,6 +17,14 @@ class Enigma
     }
   end
 
+  def decrypt(message, key = nil, date = nil)
+    {
+      decryption: msg_fuser(msg_converter_decrypt(message, key, date)) ,
+      key: key_output(key),
+      date: date_output(date)
+    }
+  end
+
   def date_output(date = nil)
     output = if date == nil
       @shift_infomation.date_today
@@ -40,6 +48,14 @@ class Enigma
     message_in_groups = msg_isolator(message)
     message_in_groups.map.with_index do |message_increment, index|
       character_converter(message_increment, shift_parameters.values)
+    end
+  end
+
+  def msg_converter_decrypt(message, key = nil, date = nil)
+    shift_parameters = @shift_infomation.shift(key, date)
+    message_in_groups = msg_isolator(message)
+    message_in_groups.map.with_index do |message_increment, index|
+      character_converter_decrypt(message_increment, shift_parameters.values)
     end
   end
 
@@ -68,6 +84,16 @@ class Enigma
     end
   end
 
-
+  def character_converter_decrypt(message_increment, shift_parameters)
+    message_increment.map.with_index do |character, index|
+      if @character_list.include?(character)
+          rotated_list = @character_list.rotate(-(shift_parameters[index]))
+          og_character_index = @character_list.index(character)
+          character = rotated_list[og_character_index]
+      else
+        character
+      end
+    end
+  end
 
 end
