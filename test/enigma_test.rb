2cb92fd68cb118f2085b49b8fc10cdbea53a650d
Pierce Alworth <pbalworth@gmail.com>
2020-01-12 09:22:16 -0700

    Adding more dynamic testing for all helper methods regarding encryption. Encryption functioning, ready to merge to master

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -23,7 +23,22 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_isolate_messages
-    assert_equal [["h", "e", "l", "l"], ["o", " ", "w", "o"], ["r", "l", "d"]], @enigma.msg_isolator("hello world")
+    expected = [["h", "e", "l", "l"], ["o", " ", "w", "o"], ["r", "l", "d"]]
+    assert_equal expected, @enigma.msg_isolator("hello world")
+  end
+
+  def test_it_can_return_character_converter
+    expected = ["k", "e", "d", "e"]
+    assert_equal expected, @enigma.character_converter((["h", "e", "l", "l"]), ([3, 27, 73, 20]))
+  end
+
+  def test_it_can_return_message_from_msg_fuser
+    assert_equal "keder ohulw", @enigma.msg_fuser([["k", "e", "d", "e"], ["r", " ", "o", "h"], ["u", "l", "w"]])
+  end
+
+  def test_it_can_return_array_of_encrypted_msg_from_msg_converter
+    expected = [["k", "e", "d", "e"], ["r", " ", "o", "h"], ["u", "l", "w"]]
+    assert_equal expected, @enigma.msg_converter("hello world", "02715", "040895")
   end
 
 end
