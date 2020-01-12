c71caa8dc44c3249b9c0267a838c017474a41abc
Pierce Alworth <pbalworth@gmail.com>
2020-01-12 10:12:36 -0700

    Adding character outside the list for more dynamic testing

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -34,11 +34,11 @@ class EnigmaTest < Minitest::Test
 
   def test_it_encrypts_message_without_key_arg
     expected = {
-      encryption: "uauhawekdhm",
+      encryption: "uauhawekdhm!",
       key: "12345",
       date: "040895"
     }
-    assert_equal expected, @enigma.encrypt("hello world", nil, "040895")
+    assert_equal expected, @enigma.encrypt("hello world!", nil, "040895")
   end
 
   def test_it_can_return_date_with_date_arg
