9cb81c523d4be4e48cb29b9eca6de851fcf3cdb8
Pierce Alworth <pbalworth@gmail.com>
2020-01-12 09:04:08 -0700

    Moving date testing to shift_test. Date bug fixed, encryption now fulling fuctional and passing test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,17 +13,14 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  def test_it_returns_date
-    assert_equal 11120, @enigma.date_today
-  end
-
   def test_it_encrypts_message
+    require "pry"; binding.pry
     expected = {
       encryption: "keder ohulw",
       key: "02715",
       date: "040895"
     }
-    assert_equal expected, @enigma.encrypt("hello world", "12715", "040895")
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
 
   def test_it_can_isolate_messages
