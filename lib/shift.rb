3c31db2918b7d51b5a909fdae2d025ee096a2f5d
Pierce Alworth <pbalworth@gmail.com>
2020-01-12 09:01:57 -0700

    Fixing date/offset bug with 0 first. Commiting lib file

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,7 +1,7 @@
 class Shift
   attr_reader :date_today, :generated_key
   def initialize
-    @date_today = Time.now.strftime("%m/%d/%y").gsub("/", "").to_i
+    @date_today = Time.now.strftime("%d/%m/%y").gsub("/", "").to_i
     @generated_key = "12345"
   end
 
